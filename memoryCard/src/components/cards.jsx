c03fab1e30ef01232d48687b0f47c7cd1cfbf2bd
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2023-09-27 15:58:36 -0300

    primeira carta alimentada por api funcionando

--- memoryCard/src/components/cards.jsx
+++ memoryCard/src/components/cards.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export function Cards({ onClick }) {
+  const [pokemon, setPokemon] = useState();
+  function getPokemon() {
+    fetch("https://pokeapi.co/api/v2/pokemon/35")
+      .then((response) => response.json())
+      .then((data) => setPokemon(String(data.sprites.front_default)));
+    //   .then((data) => setPokemon(data));
+    console.log(pokemon);
+  }
   return (
     <>
+      {getPokemon()}
       <button onClick={onClick}>
-        <img src="https://pbs.twimg.com/media/CvS_r8KWEAEYSzo?format=jpg&name=900x900"></img>
+        <img src={pokemon}></img>
         carta
       </button>
+      <button onClick={getPokemon}>TESTE</button>
     </>
   );
 }
