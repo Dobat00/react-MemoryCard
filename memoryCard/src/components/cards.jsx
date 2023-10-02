26f7d31b91e1ab53a6fe8c1e04e6eff1af589e43
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2023-10-02 16:40:14 -0300

    problemas com a geracao de cards nao foi resolvido

--- memoryCard/src/components/cards.jsx
+++ memoryCard/src/components/cards.jsx
@@ -1,22 +1,30 @@
 import { useState, useEffect } from "react";
-let pokemons = [];
 
+let pokemons = [];
 export function Cards({ onClick }) {
   const [pokemon, setPokemon] = useState();
   useEffect(() => {
     fetch(`https://pokeapi.co/api/v2/pokemon/1`)
       .then((response) => response.json())
       .then((data) => setPokemon(String(data.sprites.front_default)));
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/2`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(String(data.sprites.front_default)));
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/3`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(String(data.sprites.front_default)));
   }, []);
 
   return (
     <>
-      <button onClick={onClick}>
-        {/* {pokemons.map((poke) => {
-          return <img key={poke} src={poke}></img>;
-        })} */}
-        <img src={pokemon}></img>
-      </button>
+      <button
+        onClick={() => {
+          console.log(pokemons);
+          console.log(pokemon);
+        }}
+      ></button>
     </>
   );
 }
