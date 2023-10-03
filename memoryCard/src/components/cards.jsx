03eb1a09f39043baf4f32cf338944609658e68e6
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2023-10-03 18:01:10 -0300

    Problema na criacao de cards parcialmente resolvida,.
    Falta ser capaz de criar multiplos cards, via api

--- memoryCard/src/components/cards.jsx
+++ memoryCard/src/components/cards.jsx
@@ -1,30 +1,64 @@
 import { useState, useEffect } from "react";
 
-let pokemons = [];
+// let pokemons = [];
+// export function Cards({ onClick }) {
+//   const [pokemon, setPokemon] = useState();
+//   useEffect(() => {
+//     fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+//       .then((response) => response.json())
+//       .then((data) => pokemons.push(String(data.sprites.front_default)));
+
+//     fetch(`https://pokeapi.co/api/v2/pokemon/2`)
+//       .then((response) => response.json())
+//       .then((data) => pokemons.push(String(data.sprites.front_default)));
+//     fetch(`https://pokeapi.co/api/v2/pokemon/3`)
+//       .then((response) => response.json())
+//       .then((data) => pokemons.push(String(data.sprites.front_default)));
+//   }, []);
+
+// return (
+//   <>
+//     <button
+//       onClick={() => {
+//         console.log(pokemons[0]);
+//         console.log(pokemon);
+//       }}
+//     ></button>
+//     <button>
+//       <img src={pokemons[0]}></img>
+//     </button>
+//   </>
+// );
+// }
+
 export function Cards({ onClick }) {
   const [pokemon, setPokemon] = useState();
-  useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/1`)
-      .then((response) => response.json())
-      .then((data) => setPokemon(String(data.sprites.front_default)));
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/2`)
-      .then((response) => response.json())
-      .then((data) => setPokemon(String(data.sprites.front_default)));
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/3`)
-      .then((response) => response.json())
-      .then((data) => setPokemon(String(data.sprites.front_default)));
-  }, []);
-
+  apiFetch();
+  console.log(pokemon);
   return (
     <>
+      {createCard()}
       <button
         onClick={() => {
-          console.log(pokemons);
           console.log(pokemon);
         }}
       ></button>
     </>
   );
+  function apiFetch() {
+    fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(String(data.sprites.front_default)));
+    return pokemon;
+  }
+
+  function createCard() {
+    return (
+      <>
+        <button>
+          <img src={pokemon}></img>
+        </button>
+      </>
+    );
+  }
 }
